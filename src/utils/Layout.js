a8d130e2e9e64ccc8f8564c860744f13196e39fe
mchopan <mchopan21@gmail.com>
2023-03-15 10:34:35 +0530

    fixed bottom navbar for mobile view

--- src/utils/Layout.js
+++ src/utils/Layout.js
@@ -1,12 +1,17 @@
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer.js';
 import { Outlet } from 'react-router-dom';
+import BottomNavbar from '../components/Navbar/BottomNavbar'
+import { useTheme } from '@mui/material/styles';
 
 const Layout = ({ children }) => {
+    const theme = useTheme();
+    const isMobile = theme.breakpoints.down('sm'); // set breakpoint for mobile devices
     return (
         <>
             <Navbar />
             <Outlet />
+            {isMobile && <BottomNavbar sx={{ display: { xs: 'block', md: 'none' } }} />}
             <Footer />
         </>
     );
